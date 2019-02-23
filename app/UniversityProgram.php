1fbd3d5e82cce283d2b3221c5d097b26175056ea
Md. Masudur Rahman <masudurhimel@gmail.com>
2019-02-23 23:26:52 +0600

    universityProgram model's primary key changed

--- app/UniversityProgram.php
+++ app/UniversityProgram.php
@@ -6,5 +6,6 @@ use Illuminate\Database\Eloquent\Model;
 
 class UniversityProgram extends Model
 {
-    //
+    //as id is the foreign key and can appear multiple times
+    public $incrementing=false;
 }
