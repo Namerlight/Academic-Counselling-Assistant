45a44ff9b42b5ce72c923eb59075c0139e4a3d71
Md. Masudur Rahman <masudurhimel@gmail.com>
2019-03-10 23:39:20 +0600

    user homepage raw added

--- resources/views/pages/temporary.blade.php
+++ resources/views/pages/temporary.blade.php
@@ -1,202 +1,93 @@
-
 <!DOCTYPE html>
+<html lang="en">
 
-<!temporary implementation of the index till now>
-
-<html>
 <head>
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-
-    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
-    <link href="{{ asset('css/parallax.css') }}" rel="stylesheet">
-    <link href="{{ asset('css/modal.css') }}" rel="stylesheet">
-
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
-
-    <!For modal window modification>
-    <style type="text/css">
-
-        .modal-login {
-            width: 320px;
-        }
-
-        .modal-login .modal-content {
-            border-radius: 1px;
-            border: none;
-        }
-
-        .modal-login .modal-header {
-            position: relative;
-            justify-content: center;
-            background: #f2f2f2;
-        }
-
-        .modal-login .modal-body {
-            padding: 30px;
-        }
-
-        .modal-login .modal-footer {
-            background: #f2f2f2;
-        }
-
-        .modal-login h4 {
-            text-align: center;
-            font-size: 26px;
-        }
-
-        .modal-login label {
-            font-weight: normal;
-            font-size: 13px;
-        }
-
-        .modal-login .form-control, .modal-login .btn {
-            min-height: 38px;
-            border-radius: 2px;
-        }
-
-        .modal-login .hint-text {
-            text-align: center;
-        }
-
-        .modal-login .close {
-            position: absolute;
-            top: 15px;
-            right: 15px;
-        }
-
-        .modal-login .checkbox-inline {
-            margin-top: 12px;
-        }
-
-        .modal-login input[type="checkbox"] {
-            margin-top: 2px;
-        }
-
-        .modal-login .btn {
-            min-width: 100px;
-            background: #3498db;
-            border: none;
-            line-height: normal;
-        }
-
-        .modal-login .btn:hover, .modal-login .btn:focus {
-            background: #248bd0;
-        }
-
-        .modal-login .hint-text a {
-            color: #999;
-        }
 
 
-    </style>
+
+    <title>Simple Sidebar - Start Bootstrap Template</title>
+
+
+
+    <!-- Custom styles for this template -->
+    <link href="{{ asset('css/temp.css') }}" rel="stylesheet">
 
 </head>
+
 <body>
 
-<nav class="navbar navbar-expand-md navbar-dark fixed-top bg-dark">
+<div class="d-flex" id="wrapper">
 
-    <ul class="navbar-nav mr-auto" style="margin-left: 1%">
-        <li>
-            <a class="navbar-brand" href="/index" style="font-size: 25px;margin-left: 3%">
-                <img src="/images/logo.png" height="90%" width="10%" style="margin-top: -2%">
-                <font face="Arcitectura" style="letter-spacing: 10px; font-size: larger">ACA</font>
-            </a>
-        </li>
-    </ul>
-
-
-    <ul class="navbar-nav ml-auto">
-        <li>
-            <a href="/register" class="btn btn-primary">Sign Up</a>
-            <a id="button" class="btn btn-success" href="#myModal" data-toggle="modal">Login</a>
-        </li>
-    </ul>
-
-
-</nav>
-
-<!-- Modal HTML -->
-<div id="myModal" class="modal fade">
-    <div class="modal-dialog modal-login">
-        <div class="modal-content">
-            <form action="/examples/actions/confirmation.php" method="post">
-                <div class="modal-header">
-                    <h4 class="modal-title">Login</h4>
-                    <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
-                </div>
-                <div class="modal-body">
-                    <div class="form-group">
-                        <label>Username</label>
-                        <input type="text" class="form-control" required="required">
-                    </div>
-                    <div class="form-group">
-                        <div class="clearfix">
-                            <label>Password</label>
-                            <a href="#" class="pull-right text-muted">
-                                <small>Forgot?</small>
-                            </a>
-                        </div>
-
-                        <input type="password" class="form-control" required="required">
-                    </div>
-                </div>
-                <div class="modal-footer">
-                    <label class="checkbox-inline pull-left"><input type="checkbox"> Remember me</label>
-                    <input type="submit" class="btn btn-primary pull-right" value="Login">
-                </div>
-            </form>
+    <!-- Sidebar -->
+    <div class="bg-light border-right" id="sidebar-wrapper">
+        <div class="sidebar-heading">Start Bootstrap </div>
+        <div class="list-group list-group-flush">
+            <a href="#" class="list-group-item list-group-item-action bg-light">Dashboard</a>
+            <a href="#" class="list-group-item list-group-item-action bg-light">Shortcuts</a>
+            <a href="#" class="list-group-item list-group-item-action bg-light">Overview</a>
+            <a href="#" class="list-group-item list-group-item-action bg-light">Events</a>
+            <a href="#" class="list-group-item list-group-item-action bg-light">Profile</a>
+            <a href="#" class="list-group-item list-group-item-action bg-light">Status</a>
         </div>
     </div>
-</div>
+    <!-- /#sidebar-wrapper -->
 
-<div class="bgimg-1">
+    <!-- Page Content -->
+    <div id="">
 
+        <nav class="navbar navbar-expand-lg navbar-light bg-light border-bottom">
+            <button class="btn btn-primary" id="menu-toggle">Toggle Menu</button>
 
-    <div class="caption" style="top: 20%">
-        <h2><b>Academic Counselling Assistant</b></h2>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav ml-auto mt-2 mt-lg-0">
+                    <li class="nav-item active">
+                        <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Link</a>
+                    </li>
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                            Dropdown
+                        </a>
+                        <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+                            <a class="dropdown-item" href="#">Action</a>
+                            <a class="dropdown-item" href="#">Another action</a>
+                            <div class="dropdown-divider"></div>
+                            <a class="dropdown-item" href="#">Something else here</a>
+                        </div>
+                    </li>
+                </ul>
+            </div>
+        </nav>
+
+        <div class="container-fluid">
+            <h1 class="mt-4">Simple Sidebar</h1>
+            <p>The starting state of the menu will appear collapsed on smaller screens, and will appear non-collapsed on larger screens. When toggled using the button below, the menu will change.</p>
+            <p>Make sure to keep all page content within the <code>#page-content-wrapper</code>. The top navbar is optional, and just for demonstration. Just create an element with the <code>#menu-toggle</code> ID which will toggle the menu when clicked.</p>
+        </div>
     </div>
-</div>
+    <!-- /#page-content-wrapper -->
 
-<div style="color: #777;background-color:white;text-align:center;padding:50px 80px;text-align: justify;">
-    <h3 style="text-align:center;">Parallax Index</h3>
-    <p>Dialogues</p>
 </div>
+<!-- /#wrapper -->
 
-<div class="bgimg-2">
-    <div class="caption">
-        <span class="border" style="background-color:transparent;font-size:25px;color: #f7f7f7;">Scroll</span>
-    </div>
-</div>
+<!-- Bootstrap core JavaScript -->
+<script src="vendor/jquery/jquery.min.js"></script>
+<script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
 
-<div style="position:relative;">
-    <div style="color:#ddd;background-color:#282E34;text-align:center;padding:50px 80px;text-align: justify;">
-        <p>Scroll up and down to really get the feeling of how Parallax Scrolling works.</p>
-    </div>
-</div>
-
-<div class="bgimg-3">
-    <div class="caption">
-        <span class="border" style="background-color:transparent;font-size:25px;color: #f7f7f7;">SCROLL UP</span>
-    </div>
-</div>
-
-<div style="position:relative;">
-    <div style="color:#ddd;background-color:#282E34;text-align:center;padding:50px 80px;text-align: justify;">
-        <p>Scroll up and down to really get the feeling of how Parallax Scrolling works.</p>
-    </div>
-</div>
-
-<div class="bgimg-1">
-    <div class="caption">
-        <span class="border">COOL!</span>
-    </div>
-</div>
-
-<!Navbar shrink animation>
+<!-- Menu Toggle Script -->
 <script>
-
+    $("#menu-toggle").click(function(e) {
+        e.preventDefault();
+        $("#wrapper").toggleClass("toggled");
+    });
 </script>
 
 </body>
+
 </html>
