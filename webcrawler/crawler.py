97eade380e0bebd3c67acf0781ef761655d7e9b8
Shadab Hafiz Choudhury <shadab.choudhury@northsouth.edu>
2019-03-15 00:30:58 +0600

    webcrawler updated

--- webcrawler/crawler.py
+++ webcrawler/crawler.py
@@ -1,27 +1,39 @@
 from urllib.request import Request, urlopen
 from bs4 import BeautifulSoup
 
-page_being_crawled = 'https://www.topuniversities.com/university-rankings/world-university-rankings/2019'
+# page_being_crawled = 'https://www.topuniversities.com/university-rankings/world-university-rankings/2019'
+# req = Request(page_being_crawled, headers={'User-Agent': 'Mozilla/5.0'})
+# page_html = urlopen(req).read()
 
-req = Request(page_being_crawled, headers={'User-Agent': 'Mozilla/5.0'})
+page_being_crawled = 'https://www.timeshighereducation.com/world-university-rankings/2019/' \
+                     'world-ranking#!/page/0/length/100/sort_by/rank/sort_order/asc/cols/stats'
 
-page_html = urlopen(req).read()
-
-# page_html = urllib.request.urlopen(page_being_crawled)
+page_html = urlopen(page_being_crawled)
 
 bs_format = BeautifulSoup(page_html, 'html.parser')
 
-title = bs_format.find('h2', attrs={'class': 'element-invisible'})
+title = bs_format.find('h1', attrs={'class': 'pane-title'})
 
 test = title.text.strip()
 
 print(test)
 
-# big_table = bs_format.find('table', attrs={'class': 'dataTable no-footer'})
+table = bs_format.find('table', id='datatable-1')
 
-# my_table = big_table[0]
-# rows = my_table.findChildren(['tr'])
+name1 = table.find('a', attrs={'class': 'ranking-institution-title'})
 
-# for child in rows:
-#    print(child)
+tablebody = table.find('tbody')
 
+row1 = tablebody.find('tr', role='row')
+
+name2 = table.find('td', attrs={'class': 'name namesearch'})
+
+# <a class="ranking-institution-title" href="/world-university-rankings/university-oxford" data-position="title" data-mz="">University of Oxford</a>
+
+data1 = tablebody.find('a', attrs={'class': "ranking-institution-title", 'data-position': "title"})
+
+print(table)
+
+print(row1)
+print(name1)
+print(data1)
