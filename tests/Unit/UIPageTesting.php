8a9f66140a3142e4e729a27f7d680ef116f3d30e
Md. Masudur Rahman <masudurhimel@gmail.com>
2019-04-16 00:14:44 +0600

    unit test segment added -- android environment created

--- tests/Unit/UIPageTesting.php
+++ tests/Unit/UIPageTesting.php
@@ -2,16 +2,23 @@
 
 namespace Tests\Unit;
 
+use Illuminate\Foundation\Testing\DatabaseTransactions;
 use Tests\TestCase;
+use App\User;
 use Illuminate\Foundation\Testing\WithFaker;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 
 class UIPageTesting extends TestCase
 {
+    use DatabaseTransactions;
+
     /**
-     * A basic unit test example.
-     *
-     * @return void
+     * Testing page to test the basic pages
+     * on the user perspective
+     */
+
+    /**
+     * Index page
      */
     public function testIndexPage()
     {
@@ -20,6 +27,9 @@ class UIPageTesting extends TestCase
         $response->assertStatus(200);
     }
 
+    /**
+     * Registration page
+     */
     public function testRegisterPage()
     {
         $response = $this->get('/register');
@@ -27,10 +37,31 @@ class UIPageTesting extends TestCase
         $response->assertStatus(200);
     }
 
+    /**
+     * Main page
+     */
     public function testMainPage()
     {
         $response = $this->get('/');
 
         $response->assertStatus(200);
+    }
+
+    /**
+     * Dashboard after login
+     */
+    public function testHomepage()
+    {
+        $user = factory(User::class)->create([
+            'username' => 'tempValue'
+        ]);
+
+
+
+        $this->actingAs($user)
+            ->get('/Login/successLogin')
+            ->assertStatus(200);
+
+
     }
 }
