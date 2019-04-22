d5692ad414186ee169a5fa213f1878da16b05297
Md. Masudur Rahman <masudurhimel@gmail.com>
2019-04-22 21:06:36 +0600

    android server added

--- AndroidApp/app/src/main/java/com/example/androidapp/Constants.java
+++
@@ -1,8 +0,0 @@
-package com.example.androidapp;
-
-public class Constants {
-
-    public static final String php_functions = "http://192.168.1.193/nsu.spring2019.cse327.1.t4/AndroidApp/servers/";
-    public static final String login_function = php_functions + "Login.php";
-    
-}
